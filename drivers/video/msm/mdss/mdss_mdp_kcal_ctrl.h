b12f025766342b7c38210cd467a6be7766c9c835
HolyAngel <slverwolf@gmail.com>
2015-03-12 07:56:38 -0500

    Revert "msm: mdss: Add hue/contrast support to kcal"
    This reverts commit 34fc4193caafb1852fbe300c06dafeec68e85dab.

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.h
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.h
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  * Copyright (c) 2013, LGE Inc. All rights reserved
@@ -32,12 +31,9 @@ struct kcal_lut_data {
 	int enable;
 	int invert;
 	int sat;
-	int hue;
-	int val;
-	int cont;
 };
 
 void mdss_mdp_pp_kcal_enable(bool enable);
 void mdss_mdp_pp_kcal_update(int kr, int kg, int kb);
-void mdss_mdp_pp_kcal_pa(struct kcal_lut_data *lut_data);
+void mdss_mdp_pp_kcal_sat(int level);
 #endif
